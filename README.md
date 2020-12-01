e63f3a7cb30a09552051ce9453a6249d878a4d41
skyclo <hello.skyclo@gmail.com>
2020-12-01 08:30:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# harmony-scripts
+Scripts that allow Git Repositories to play nicely with the Unity3D editor.
+# flightdemo3
+# flightdemo3
---
+++ harmony.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Move to the root directory of your Unity3D project
+# Then run it with sudo perms
+
+# Gets Repo Name Input
+read -p "Repo Name: " reponame
+
+# Copies the gitignore file into the root directory
+cp ./harmony-assets/Unity.gitignore ./.gitignore
+
+# Copies the gitattributes file into the root directory
+cp ./harmony-assets/Unity.gitattributes ./.gitattributes
+
+# Installs Git LFS
+sudo apt install git-lfs
+
+# Git setup
+echo "# $reponame" >> README.md
+git init
+git add *
+git commit -m "Initial commit"
+git branch -M main
+git remote add origin https://github.com/skyclo/$reponame
+git remote -v
+git push -u origin main
+
+# Done!
+echo
+echo Done!
+
