4d97d7bf52446f20d4b144a569fe80e0c9752143
skyclo <hello.skyclo@gmail.com>
2020-12-09 08:43:26 -0500

    Added more properties/methods to AtmosAircraft

--- Assets/Scripts/Atmos/AtmosAircraft.cs
+++ Assets/Scripts/Atmos/AtmosAircraft.cs
@@ -14,10 +14,10 @@ public class AtmosAircraft : MonoBehaviour
     public float initialFuelLoad = 1f;
     private float remainingFuel;
     /* -------------------------------------------- Wings ------------------------------------------- */
-    public AtmosControlSurface[] elevons;
-    public AtmosControlSurface[] alerion;
-    public AtmosControlSurface[] rudders;
-    public AtmosFixedWing[] fixedWings;
+    public List<AtmosControlSurface> elevons;
+    public List<AtmosControlSurface> alerion;
+    public List<AtmosControlSurface> rudders;
+    public List<AtmosFixedWing> fixedWings;
     /* ------------------------------------------- Forces ------------------------------------------- */
     private Vector3 appliedForce;
     private Vector3 thrustForce;
@@ -25,6 +25,7 @@ public class AtmosAircraft : MonoBehaviour
     private Vector3 dragForce;
     private Vector3 gravityForce;
     /* --------------------------------------- Thruster Config -------------------------------------- */
+    public List<AtmosThruster> thrusters;
     public float maxEngineThrust;
     public float maxABThrust;
     public float throttleRange = 1.0f;
@@ -67,4 +68,40 @@ public class AtmosAircraft : MonoBehaviour
     }
 
     // Deflect Control Surfaces is called every FixedUpdate and moves all ControlSurfaces
+    void DeflectControlSurfaces()
+    {
+
+    }
+
+    // Calculate Control Surfaces is called every FixedUpdate and adds lift vectors
+    void CalculateLiftForAllSurfaces()
+    {
+
+    }
+
+    // Calculate Control Surfaces is called every FixedUpdate and adds drag vectors
+    void CalculateDragForAllSurfaces() {
+
+    }
+
+    // Calculate Drag is called every FixedUpdate and adds all drag for all non-CS/FW objects
+    void CalculateDrag()
+    {
+
+    }
+
+    // Calculate Thrust is called every FixedUpdate and adds all thrust from the engines
+    void CalculateThrust() {
+
+    }
+
+    // Calculate Gravity is called every Fixed Update and adds the rigidbody's current mass
+    void CalculateGravity() {
+
+    }
+
+    // Calculate Forces is called every fixed update and combines all force equations
+    void CalculateForces() {
+        
+    }
 }
