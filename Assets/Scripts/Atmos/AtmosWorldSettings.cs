bc6a94c6d2f00aa98adb9e0e4ac02777856c3506
skyclo <hello.skyclo@gmail.com>
2020-12-09 08:43:43 -0500

    Added AtmosWorldSettings class

---
+++ Assets/Scripts/Atmos/AtmosWorldSettings.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class AtmosWorldSettings
+{
+
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                         CLASS VARIABLES                                        */
+    /* ---------------------------------------------------------------------------------------------- */
+    public static float airDensity = 1.229f;
+    public static float airPressure = 0f; // Not implimented yet // TODO
+    public static float gravitationalConstant = 9.81f;
+
+    /* ---------------------------------------------------------------------------------------------- */
+    /*                                             METHODS                                            */
+    /* ---------------------------------------------------------------------------------------------- */
+    // none
+}
